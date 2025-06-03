4389dee39dafff9253c2f4ecd1f02a46834102e8
Silvia edith Cortes <jorge@cbtis66.edu.mx>
2025-06-03 13:09:32 -0600

    allowed

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require('express');
 require('dotenv').config();
+const cors = require('cors');
 
 const app = express();
 const librosRoutes = require('./routes/Libros');
+
+const allowedOrigins = ['http://localhost' ];
+
+
+app.use(cors({
+    origin: (origin, callback)=> {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+           
+        callback(null, true);
+      } else {
+        console.log("Dominio no permitido");
+        callback(new Error("Este dominio no esta permitido"));
+      }
+    } 
+  }));
 
 app.use(express.json());
 app.use('/api/libros', librosRoutes);
