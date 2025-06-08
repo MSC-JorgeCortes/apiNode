da11f8f785d870ea8317d6340cfa4c9844e70f47
Jorge Cortés Domínguez <jorge@cbtis66.edu.mx>
2025-06-08 11:33:28 -0600

    eee

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const allowedOrigins = ['http://localhost:5500','http://127.0.0.1:5500',];
 app.use(cors({
     origin: (origin, callback)=> {
       if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-           
         callback(null, true);
       } else {
         console.log("Dominio no permitido");
