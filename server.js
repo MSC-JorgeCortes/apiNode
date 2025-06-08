4a33e7179164f697dc8bbd8c42037b5c6b686212
JORGE CORTES DOMINGUEZ <jorge@cbtis66.edu.mx>
2025-06-08 11:41:59 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const cors = require('cors');
 
 const app = express();
-const librosRoutes = require('./routes/Libros.js');
+const librosRoutes = require('./routes/Libros');
 
 const allowedOrigins = ['http://localhost:5500','http://127.0.0.1:5500',];
 //ejercicio para aplicar un branch en git la  rama se llama ejemplo1
